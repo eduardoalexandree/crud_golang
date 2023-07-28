53d7b6eb978b3cb386e06239fd17a1b9e0f5c232
eduardoalexandree <90580475+eduardoalexandree@users.noreply.github.com>
2023-07-28 17:11:56 -0300

    get coasterHandlers

--- crud_net/go.mod
+++ crud_net/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.11 // indirect
 	github.com/radovskyb/watcher v1.0.7 // indirect
--- crud_net/main.go
+++ crud_net/main.go
@@ -1,13 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world 2"))
-	})
-	http.ListenAndServe(":3000", nil)
+type Coaster struct {
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"inPark"`
+	Height       int    `json:"height"`
+}
 
+type coasterHandlers struct {
+	store map[string]Coaster
+}
+
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
+	coasters := make([]Coaster, len(h.store))
+
+	i := 0
+
+	for _, coaster := range h.store {
+		coasters[i] = coaster
+		i++
+	}
+
+	jsonBytes, err := json.Marshal(coasters)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+
+}
+
+func newCoasterHandlers() *coasterHandlers {
+	return &coasterHandlers{
+		store: map[string]Coaster{
+			"id1": Coaster{
+				Name:         "fury",
+				Height:       99,
+				ID:           "id1",
+				InPark:       "Carowinds",
+				Manufacturer: "B+M",
+			},
+		},
+	}
+}
+
+func main() {
+	coasterHandlers := newCoasterHandlers()
+	http.HandleFunc("/coasters", coasterHandlers.get)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
